6e01a7130e810ee7b18f4e09bf5ce031cffe037d
Yue <cathywang428@gmail.com>
2022-01-21 18:49:32 -0600

    flask

---
+++ greet/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask('__home__')
+
+@app.route('/welcome')
+def welcome_page():
+    return '<h1>Welcome</h1>'
+
+@app.route('/welcome/home')
+def welcome_home():
+    return '<h1>Welcome Home</h1>'
+
+@app.route('/welcome/back')
+def welcome_back():
+    return '<h1>Welcome Back</h1>'
